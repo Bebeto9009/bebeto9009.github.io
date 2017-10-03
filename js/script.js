92869ab3eee19257deffb13d4d601f2b1e75deca
Bebeto9009 <adr.pawlak@gmail.com>
2017-10-03 12:12:59 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function changeContentONas() {
+	let otherContent = document.getElementById("content1");
+	otherContent.innerHTML = "<p>W tym serwisie sprawdzisz aktualną pogodę dla wybranych miast województwa lubelskiego</p>";
+}
+
+
+function changeContentLublin() {
+	let otherContent = document.getElementById("content1");
+	otherContent.innerHTML = "<p>W tym serwisie sprawdzisz aktualną pogodę dla miasta Lublina</p>";
+}
+
+
+function changeContentZamość() {
+	let otherContent = document.getElementById("content1");
+	otherContent.innerHTML = "<p>Pogoda dla miasta Zamość w przygotowaniu</p>";
+}
+
+
+function changeContentŚwidnik() {
+	let otherContent = document.getElementById("content1");
+	otherContent.innerHTML = "<p>Pogoda dla miasta Świdnika w przygotowaniu</p>";
+}
